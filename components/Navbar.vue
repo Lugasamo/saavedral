9393c93ae96c780084331b75daa0155af4ef8681
Luis Saavedra <luis@saavedral.me>
2018-02-24 16:07:37 +1100

    Added menu styles and modifications.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,23 +1,86 @@
 <template>
-  <div class="container">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">Portfolio</a>
+    <md-list>
+        <md-list-item>
+            <nuxt-link class="navbar-item" to="/">
+                <img src="~/assets/images/saavedral.svg"/>
+                <div class="md-divider"></div>
+            </nuxt-link>
+        </md-list-item>
 
-        <button class="button navbar-burger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-      </div>
+        <md-list-item>
+            <nuxt-link class="navbar-item" to="/about">
+                <h1 class="md-subheading">About me.</h1>
+                <div class="md-body-1">A bit of me.</div>
+            </nuxt-link>
+        </md-list-item>
 
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
-          <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
-          <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
-        </div>
-      </div>
-    </nav>
-  </div>
+        <md-list-item>
+            <nuxt-link class="navbar-item" to="/projects">
+                <h1 class="md-subheading">Portfolio.</h1>
+                <div class="md-body-1">My work in the industry.</div>
+            </nuxt-link>
+        </md-list-item>
+
+        <md-list-item>
+            <nuxt-link class="navbar-item" to="/contact">
+                <h1 class="md-subheading">Contact.</h1>
+                <div class="md-body-1"> I would love to hear from you.</div>
+            </nuxt-link>
+        </md-list-item>
+
+        <md-app-content>
+            <div class="md-divider"></div>
+            <md-list class="social-media">
+                <md-avatar class="md-avatar-icon">
+                    <a href="https://www.linkedin.com/in/luis-g-saavedra" target="_blank"><i class="fa fa-linkedin"></i></a>
+                </md-avatar>
+
+                <md-avatar class="md-avatar-icon">
+                    <a href="https://github.com/Lugasamo" target="_blank"><i class="fa fa-github"></i></a>
+                </md-avatar>
+
+                <md-avatar class="md-avatar-icon">
+                    <a href="https://gitlab.com/Saavedral" target="_blank"><i class="fa fa-gitlab"></i></a>
+                </md-avatar>
+
+                <md-avatar class="md-avatar-icon">
+                    <a href="https://www.drupal.org/u/lugasamo" target="_blank"><i class="fa fa-drupal"></i></a>
+                </md-avatar>
+
+            </md-list>
+        </md-app-content>
+    </md-list>
 </template>
+
+<style lang="scss" scoped>
+    @import '~assets/scss/styles.scss';
+    .md-list {
+        .md-subheading {
+            margin-bottom: $space--sm;
+        }
+        .md-body-1 {
+            color: $color__third-blue;
+            font-weight: $font__weight--medium;
+        }
+        a {
+            text-decoration: none;
+            &:visited {
+                color: inherit;
+            }
+        }
+    }
+    .social-media {
+        display: inline;
+        .md-avatar-icon {
+            background: $color__secondary-blue;
+            color: $color__white;
+            margin: $space--xs;
+            :visited {
+                color: $color__white;
+            }
+            :first-child {
+                margin-left: 0;
+            }
+        }
+    }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,86 @@
 <template>
-  <div>
-    <navbar></navbar>
-    <nuxt/>
-  </div>
+    <div>
+        <md-app md-mode="reveal">
+            <md-app-toolbar class="md-primary">
+                <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
+                    <md-icon>menu</md-icon>
+                </md-button>
+            </md-app-toolbar>
+
+            <md-app-drawer :md-active.sync="menuVisible" md-persistent="full">
+                <md-toolbar class="md-transparent" md-elevation="0">
+                    <span>Navigation</span>
+
+                    <div class="md-toolbar-section-end">
+                        <md-button class="md-icon-button md-dense" @click="toggleMenu">
+                            <md-icon>keyboard_arrow_left</md-icon>
+                        </md-button>
+                    </div>
+                </md-toolbar>
+
+                <navbar></navbar>
+
+            </md-app-drawer>
+            <md-app-content>
+                <nuxt/>
+            </md-app-content>
+        </md-app>
+    </div>
 </template>
 
 <script>
-  import Vue from 'vue'
-  import VueMaterial from 'vue-material'
+    import Navbar from '../components/Navbar'
+    import Vue from 'vue'
+    import VueMaterial from 'vue-material'
+    import 'vue-material/dist/vue-material.css'
 
-  import 'vue-material/dist/vue-material.css'
-  import 'assets/scss/styles.scss'
+    import 'assets/scss/styles.scss'
 
-  import Navbar from '../components/Navbar'
+    Vue.use(VueMaterial)
 
-  Vue.use(VueMaterial)
-  export default {
-    components: {
-      Navbar
+    export default {
+      name: 'PersistentFull',
+      components: {
+        Navbar
+      },
+      data: () => ({
+        menuVisible: false
+      }),
+      methods: {
+        toggleMenu () {
+          this.menuVisible = !this.menuVisible
+        }
+      }
     }
-  }
 </script>
 
-<style>
-    html {
-    font-family: 'Nunito', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  }
-</style>
+<style lang="scss" scoped>
+    @import '../assets/scss/styles';
 
+    html {
+        font-family: 'Nunito', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    }
+    .md-app {
+        height: -webkit-fill-available;
+        padding-top: $space--sm;
+    }
+    .md-app-content {
+        padding: 0 $space--md;
+    }
+    .md-toolbar {
+        box-shadow: none;
+        .md-button {
+            background: $color__science-blue;
+        }
+    }
+    .md-drawer {
+        @include md-layout-medium-and-up {
+            width: 400px;
+        }
+        background: $color__white;
+        color: $color__secondary-blue;
+        overflow: hidden;
+        position: fixed;
+        width: 100%;
+    }
+</style>
